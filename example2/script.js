446dd87bb71b77c2ac77714a9e3ca4107c642fbd
sumerian12345 <76828889+sumerian12345@users.noreply.github.com>
2021-02-07 15:48:46 -0700

    L3_E2_Sumer Matharu
    Modified example 2 for lecture 3 by Sumer Matharu @pearswj @fraguada
    
    Co-Authored-By: Will Pearson <121068+pearswj@users.noreply.github.com>
    Co-Authored-By: Luis E. Fraguada <fraguada@gmail.com>

--- example2/script.js
+++ example2/script.js
@@ -5,13 +5,14 @@ import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm
 import { RhinoCompute } from 'https://cdn.jsdelivr.net/npm/compute-rhino3d@0.13.0-beta/compute.rhino3d.module.js'
 
 // reference the definition
-const definitionName = 'rnd_node.gh'
+const definitionName = 'Fractal_cubes_2.gh'
+
 
 // listen for slider change events
-const count_slider = document.getElementById( 'count' )
-count_slider.addEventListener( 'input', onSliderChange, false )
-const radius_slider = document.getElementById( 'radius' )
-radius_slider.addEventListener( 'input', onSliderChange, false )
+const step_slider = document.getElementById( 'step' )
+step_slider.addEventListener( 'input', onSliderChange, false )
+const size = document.getElementById( 'size' )
+size.addEventListener( 'input', onSliderChange, false )
 
 const downloadButton = document.getElementById("downloadButton")
 downloadButton.onclick = download
@@ -23,6 +24,13 @@ loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 // create a few variables to store a reference to the rhino3dm library and to the loaded definition
 let rhino, definition, doc
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+
 rhino3dm().then(async m => {
     rhino = m
 
@@ -32,6 +40,8 @@ rhino3dm().then(async m => {
     // remote
     RhinoCompute.url = 'https://macad2021.compute.rhino3d.com/'
     RhinoCompute.apiKey = getApiKey() // needed when calling a remote RhinoCompute server
+
+ 
 
     // source a .gh/.ghx file in the same directory
     let url = definitionName
@@ -49,14 +59,14 @@ async function compute() {
     // collect data
 
     // get slider values
-    let count = document.getElementById('count').valueAsNumber
-    let radius = document.getElementById('radius').valueAsNumber
+    let step = document.getElementById('step').valueAsNumber
+    let size = document.getElementById('size').valueAsNumber
 
     // format data
-    let param1 = new RhinoCompute.Grasshopper.DataTree('RH_IN:radius')
-    param1.append([0], [radius])
-    let param2 = new RhinoCompute.Grasshopper.DataTree('RH_IN:count')
-    param2.append([0], [count])
+    let param1 = new RhinoCompute.Grasshopper.DataTree('RH_IN:size')
+    param1.append([0], [size])
+    let param2 = new RhinoCompute.Grasshopper.DataTree('RH_IN:step')
+    param2.append([0], [step])
 
     // Add all params to an array
     let trees = []
@@ -88,6 +98,7 @@ function collectResults(values) {
 
     console.log(values)
     doc = new rhino.File3dm()
+
 
     for ( let i = 0; i < values.length; i ++ ) {
 
@@ -144,7 +155,7 @@ function getApiKey() {
 // download button handler
 function download () {
     let buffer = doc.toByteArray()
-    saveByteArray("node.3dm", buffer)
+    saveByteArray("yourfractalismycommand.3dm", buffer)
 }
 
 function saveByteArray ( fileName, byte ) {
@@ -163,9 +174,9 @@ function init() {
 
     // create a scene and a camera
     scene = new THREE.Scene()
-    scene.background = new THREE.Color(1, 1, 1)
+    scene.background = new THREE.Color(0, 0, 0)
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.z = - 30
+    camera.position.z = - 250
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -176,12 +187,13 @@ function init() {
     const controls = new OrbitControls(camera, renderer.domElement)
 
     // add a directional light
-    const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    const directionalLight = new THREE.DirectionalLight( 0xE50914 )
     directionalLight.intensity = 2
     scene.add( directionalLight )
 
     const ambientLight = new THREE.AmbientLight()
     scene.add( ambientLight )
+
 
 }
 
